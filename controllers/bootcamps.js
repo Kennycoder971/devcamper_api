742823703823c3a9dc16be95dd61e676588a417a
kenny <kennycoder971@gmail.com>
2022-05-17 15:13:26 +0200

    Adding routes, middlewares and logger

---
+++ controllers/bootcamps.js
@@ -0,0 +1,45 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: "true", message: "Hi" });
+};
+
+// @desc    Get a bootcamps
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: "true", message: `Get bootcamp ${req.params.id}` });
+};
+
+// @desc    Create new bootcamps
+// @route   POST /api/v1/bootcamps
+// @access  Private
+
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: "true", message: `Create bootcamp` });
+};
+
+// @desc    Update bootcamps
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: "true", message: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc    Delete bootcamps
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: "true", message: `Delete bootcamp ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+// Route files
+const bootcamp = require("./routes/bootcamps");
 
 // Load env vars
-
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamp);
+
 const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
 
 app.listen(
   PORT,
