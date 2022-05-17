a883d331968566d111f6adc7f6a171f0a3185682
kenny <kennycoder971@gmail.com>
2022-05-17 18:04:06 +0200

    Creating models

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamp = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect Database
+connectDB();
+
+// Route files
+const bootcamp = require("./routes/bootcamps");
 
 const app = express();
 
@@ -24,7 +29,17 @@ app.get("/", (req, res) => {
   res.send("Hello from express");
 });
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
