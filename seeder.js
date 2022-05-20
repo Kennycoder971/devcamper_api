bd4c9247849f01933824eb11e476cbcf42545ba3
kenny <kennycoder971@gmail.com>
2022-05-20 17:42:31 +0200

    Geocode middleware and course routers

---
+++ seeder.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env variables
+
+dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Bootcamps = require("./models/Bootcamp");
+const Courses = require("./models/Course");
+
+// Connect to DB
+
+mongoose.connect(process.env.MONGO_URI);
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamps.create(bootcamps);
+    await Courses.create(courses);
+    console.log("Data Imported".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Delete data
+
+// Import into DB
+const deleteData = async () => {
+  try {
+    await Bootcamps.deleteMany();
+    await Courses.deleteMany();
+    console.log("Data Destroyed".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
