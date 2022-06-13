3036686d73fa288fe4a6e719d0ccce040bfba701
kenny <kennycoder971@gmail.com>
2022-06-13 17:27:24 +0200

    Creating auth controller

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -14,6 +16,7 @@ connectDB();
 // Route files
 const bootcamp = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -25,9 +28,16 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// File uploading
+app.use(fileUpload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamp);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
