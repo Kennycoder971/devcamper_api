55acf1ca3aa0859360dacdca3fc1e2d280c6fd60
kenny <kennycoder971@gmail.com>
2022-06-20 12:52:30 +0200

    Autorization middleware

--- seeder.js
+++ seeder.js
@@ -8,11 +8,11 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 // Load models
-const Bootcamps = require("./models/Bootcamp");
-const Courses = require("./models/Course");
+const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
+const User = require("./models/User");
 
 // Connect to DB
-
 mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
@@ -22,12 +22,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 // Import into DB
 const importData = async () => {
   try {
-    await Bootcamps.create(bootcamps);
-    await Courses.create(courses);
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    await User.create(users);
     console.log("Data Imported".green.inverse);
     process.exit();
   } catch (error) {
@@ -38,8 +42,9 @@ const importData = async () => {
 // Delete data
 const deleteData = async () => {
   try {
-    await Bootcamps.deleteMany();
-    await Courses.deleteMany();
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data Destroyed".red.inverse);
     process.exit();
   } catch (error) {
